e4fc41fe2a8ed072dc8766397e222f4386dd743b
ayush.pahuja <ayush.pahuja090@gmail.com>
2022-05-29 01:58:53 +0530

    Deploying web3 complete

--- Web3_py_storage/compler.py
+++ Web3_py_storage/compler.py
@@ -1,28 +1,101 @@
-from charset_normalizer import from_path
+import json
+
+from web3 import Web3
+
+# In the video, we forget to `install_solc`
+# from solcx import compile_standard
+from solcx import compile_standard, install_solc
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-from solc import compile_standard
-
-with open("./SimpleStorage", "r") as file:
+with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
 
-#adding the compiler for solidity
+# We add these two lines that we forgot from the video!
+print("Installing...")
+install_solc("0.6.0")
 
+# Solidity source code
 compiled_sol = compile_standard(
     {
         "language": "Solidity",
-        "sources": {"SimpleStorage.sol":{"content":simple_storage_file}},
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
         "settings": {
-            "outputSelection":{
-                "*":{
-                    "*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]
-
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
                 }
             }
-
-        }
-
-    }
-    solc_version="0.6.0"
+        },
+    },
+    solc_version="0.6.0",
 )
-print(compiled_sol)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = json.loads(
+    compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
+)["output"]["abi"]
+
+# w3 = Web3(Web3.HTTPProvider(os.getenv("RINKEBY_RPC_URL")))
+# chain_id = 4
+#
+# For connecting to ganache
+w3 = Web3(Web3.HTTPProvider("http://0.0.0.0:8545"))
+chain_id = 1337
+my_address = "0xdbB4A708755dfD59f9c4b100B2BE23a6d2EB7D57"
+private_key = "ffdd7a010ab8c089d95a9c2ff24e75b21744b5db26c3cd66d14f8e91c46afcc4"
+
+# Create the contract in Python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+# Get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+# Submit the transaction that deploys the contract
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+# Sign the transaction
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+print("Deploying Contract!")
+# Send it!
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+# Wait for the transaction to be mined, and get the transaction receipt
+print("Waiting for transaction to finish...")
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
+
+
+# Working with deployed Contracts
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+print(f"Initial Stored Value {simple_storage.functions.retrieve().call()}")
+greeting_transaction = simple_storage.functions.store(15).buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce + 1,
+    }
+)
+signed_greeting_txn = w3.eth.account.sign_transaction(
+    greeting_transaction, private_key=private_key
+)
+tx_greeting_hash = w3.eth.send_raw_transaction(signed_greeting_txn.rawTransaction)
+print("Updating stored Value...")
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_greeting_hash)
+
+print(simple_storage.functions.retrieve().call())
